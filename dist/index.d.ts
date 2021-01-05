8ffe32f0cbb6b14e1bf0db0e8df1924b75b72d44
Turkyden <wj871287@gmail.com>
2021-01-05 18:13:36 +0800

    ddd

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { FC, HTMLAttributes, ReactChild } from 'react';
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-    /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-    children?: ReactChild;
-}
-/**
- * A custom Thing component. Neat!
- */
-export declare const Thing: FC<Props>;
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as useDarkmode } from '@/use-darkmode';
