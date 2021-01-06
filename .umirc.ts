f897eb098636cb4a5da037d3917a6f01aa3dda7e
Turkyden <616287091@qq.com>
2021-01-06 08:16:57 +0800

    feat: fix the public path

--- .umirc.ts
+++ .umirc.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   hash: true,
   outputPath: 'docs-dist',
   base: `/react-darkreader/`,
+  publicPath: '/react-darkreader/',
   // publicPath:
   //   process.env.NODE_ENV === 'production'
   //     ? 'https://cdn.jsdelivr.net/gh/turkyden/react-darkreader@gh-pages/'
