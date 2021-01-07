ed71a0d83a520e95bd4c2053b735e978878d0357
Turkyden <616287091@qq.com>
2021-01-08 01:21:13 +0800

    ci: fix the ci

--- .umirc.ts
+++ .umirc.ts
@@ -8,14 +8,7 @@ export default defineConfig({
   outputPath: 'docs-dist',
   base: `/react-darkreader/`,
   publicPath:
-    process.env.NODE_ENV === 'production'
-      ? 'https://cdn.jsdelivr.net/gh/turkyden/react-darkreader@gh-pages/'
-      : '/',
-  // publicPath:
-  //   process.env.NODE_ENV === 'production'
-  //     ? 'https://cdn.jsdelivr.net/gh/turkyden/react-darkreader@gh-pages/'
-  //     : '/',
-  // more config: https://d.umijs.org/config
+    process.env.NODE_ENV === 'production' ? '/react-darkreader/' : '/',
   theme: {
     '@c-primary': '#2c89a0',
   },
