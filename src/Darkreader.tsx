7e413211e10eb6852b5a8159510d0e1db252f94d
chenshuai2144 <qixian.cs@outlook.com>
2021-02-24 10:31:51 +0800

    feat: add onChange props

--- src/Darkreader.tsx
+++ src/Darkreader.tsx
@@ -5,13 +5,23 @@ import Switch from './Switch';
 export interface DarkreaderProps {
   defaultDarken?: boolean;
   options?: Options;
+  onChange?: (checked: boolean) => void;
 }
 
 export default function Darkreader({
   defaultDarken = false,
   options,
+  onChange,
 }: DarkreaderProps) {
   const [isDark, { toggle }] = useDarkreader(defaultDarken, options);
 
-  return <Switch checked={isDark} onChange={toggle} />;
+  return (
+    <Switch
+      checked={isDark}
+      onChange={checked => {
+        toggle();
+        onChange?.(checked);
+      }}
+    />
+  );
 }
